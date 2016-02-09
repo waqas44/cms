21fd9a5597b0875171254ac23ee33a121ddb05b0
unknown <wiki.freelancer@gmail.com>
2016-02-09 23:55:30 +0500

    revert header

--- header.php
+++ header.php
@@ -54,10 +54,7 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
      
-      <?php // checking if user is login 
-      
-      
-      if(isset($_SESSION['name'])
+      <?php if(isset($_SESSION['name'])
 		){?>  <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION['name'] ?><span class="caret"></span></a>
           <ul class="dropdown-menu">
